a2afc577a721ba0cbd5a4caec75b847777c2537e
jomariepolanco <jomariepolanco@gmail.com>
2020-11-16 17:15:20 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.body.querySelector('main')
+main.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = "Jomarie is the champion"
